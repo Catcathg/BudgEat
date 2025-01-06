112f78c82d98bd646d3054d90d0824ffe49ef951
Catcathg <catherine-guo@outlook.fr>
2025-01-06 23:02:25 +0100

    map image + centrer

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -1,5 +1,4 @@
 <?php
-// src/Controller/MapController.php
 
 namespace App\Controller;
 
@@ -17,18 +16,15 @@ class MapController extends AbstractController
      */
     public function showRestaurant(int $id, RestaurantsRepository $restaurantsRepository, HttpClientInterface $client): Response
     {
-        // Récupérer le restaurant avec l'ID donné
+
         $restaurant = $restaurantsRepository->find($id);
 
-        // Si le restaurant n'existe pas, on lance une exception 404
         if (!$restaurant) {
             throw $this->createNotFoundException('Restaurant non trouvé');
         }
+        
+        $adresse = $restaurant->getAdresse();
 
-        // Créer l'adresse complète du restaurant
-        $adresse = $restaurant->getAdresse() . ', ' . $restaurant->getVille() . ' ' . $restaurant->getCodePostal();
-
-        // Envoyer la requête à l'API Nominatim pour obtenir les coordonnées
         $response = $client->request('GET', 'https://nominatim.openstreetmap.org/search', [
             'query' => [
                 'q' => $adresse,
@@ -38,19 +34,15 @@ class MapController extends AbstractController
             ]
         ]);
 
-        // Vérifier que la réponse contient des données
         $data = $response->toArray();
 
-        // Si l'API retourne des résultats, on récupère la latitude et la longitude
         if (!empty($data) && isset($data[0]['lat'], $data[0]['lon'])) {
             $latitude = $data[0]['lat'];
             $longitude = $data[0]['lon'];
         } else {
-            // Si aucune coordonnée n'est trouvée, on lance une exception
             throw $this->createNotFoundException('Coordonnées non trouvées pour l\'adresse du restaurant');
         }
 
-        // Passer les données au template
         return $this->render('map/index.html.twig', [
             'commander' => $restaurant,
             'latitude' => $latitude,
@@ -58,3 +50,4 @@ class MapController extends AbstractController
         ]);
     }
 }
+
